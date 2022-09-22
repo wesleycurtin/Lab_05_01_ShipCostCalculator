a4a190459de2966afbab46568b50335bd6914d1a
WesleyCurtin <curtinwl@mail.uc.edu>
2022-09-22 13:19:53 -0400

    Lab 5 Project 1 End commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,27 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int cost = 274;
+        double shipping = 0.00;
+        double totalPrice = 0.00;
+
+
+
+        if (cost >= 100) {
+            System.out.println("You qualify for free shipping");
+        }   else {
+                shipping = cost * 0.02;
+                }
+
+        totalPrice = cost + shipping;
+
+        System.out.println(("Your cost for shipping is ") + shipping);
+        System.out.println(("Therefore your total price is ") + totalPrice);
+
+
+
+
+
+
     }
 }
